5ad0fe33f153412b70185a86a09eb9a19b560cba
jomote-florida <jomote@alumnatflorida.es>
2024-12-06 18:04:20 +0100

    modificacion correctiva de paginas.js

--- pagina1.js
+++ pagina1.js
@@ -1,2 +1,3 @@
 console.log("Aqui está un segundo archivo js que subo para comprobar que todo va correcto");
 console.log("Aqui le he metido una modificacion a la vez que voy a crear otro documento");
+console.log("YA que al haber modificado y creado a la vez, el mensaje del commit queda muy raro, así que ambos serán modificados")
--- pagina2.js
+++ pagina2.js
@@ -1,1 +1,2 @@
 console.log("Aquí creando otra pagina para hacer otro commit y push!");
+console.log("Aqui voy a ahcer una modificacion junto a pagina1.js para que el mensaje del commit sea más acertado");
