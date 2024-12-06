8c4e349f19391fef8c162e3e0fc98cf24e9d112f
jomote-florida <jomote@alumnatflorida.es>
2024-12-06 17:49:42 +0100

    Creacion de index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hola mundo de GitHub")
+console.log("Este es mi primer commit y push desde visual studio code");
+console.log("Y ahora me pregunto por que no aparece este archivo jaja");
